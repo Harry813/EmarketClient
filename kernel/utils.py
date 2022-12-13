0331f6f958eb359538f8928c56e87dd2fc99486a
HarryW <wangaoharry@hotmail.com>
2022-12-13 14:07:23 -0800

    Update:   - Add Utils

---
+++ kernel/utils.py
@@ -0,0 +1,72 @@
+import json
+import os
+
+import requests as rq
+
+from kernel.models import *
+
+
+def send_request(url, method, data=None, headers=None):
+    """
+    Send request to url
+    :param url: url path to send request (e.g. /path/to/api)
+    :param method: request method ["GET", "POST", "PUT", "DELETE"]
+    :param data: data to send in format of dictionary, required by POST and PUT
+    :param headers: headers to send
+    """
+    headers = {
+        "X-Api-Key": os.getenv("API_KEY"),
+        **headers
+    }
+    url = f"{os.getenv('API_ROOT')}{url}"
+    data = json.dumps(data)
+
+    if method == "GET":
+        return rq.get(url, headers=headers)
+    elif method == "POST":
+        return rq.post(url, data=data, headers=headers)
+    elif method == "PUT":
+        return rq.put(url, data=data, headers=headers)
+    elif method == "DELETE":
+        return rq.delete(url, headers=headers)
+    else:
+        return None
+
+
+def create_user(**kwargs):
+    """
+    Create user with username, password, email, first_name, last_name
+    Please check the password format before creation
+    """
+    username = kwargs.get("username")
+    email = kwargs.get("email")
+    password = kwargs.get("password")
+    first_name = kwargs.get("first_name")
+    last_name = kwargs.get("last_name")
+
+    user = User.objects.create_user(username=username,
+                                    email=email,
+                                    password=password,
+                                    first_name=first_name,
+                                    last_name=last_name)
+    user.save()
+    response = send_request("/auth/register/", "POST",
+                            {"username": username, "email": email,
+                             "first_name": first_name, "last_name": last_name})
+    if response.status_code == 201:
+        return user
+    else:
+        user.delete()
+        return None
+
+
+def v_record(requst):
+    data = {"ip": requst.META["REMOTE_ADDR"], "site": requst.META["SERVER_NAME"], "url": requst.path,
+            "useragent": requst.META["HTTP_USER_AGENT"]}
+    if requst.user.is_authenticated:
+        data["user"] = requst.user.pk
+    UserVisitRecord.objects.create(
+        ip=data["ip"],
+        user_id=data["user"] if "user" in data else None,
+        site=data["site"],
+    )
