9479f8d8bbd0c06f38fd3f0c875ee311ce2d91c0
HarryW <wangaoharry@hotmail.com>
2022-12-14 01:28:28 -0800

    Update:   - Add client test view

--- client/views.py
+++ client/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
+from kern.utils import send_request
+
 
 # Create your views here.
+def test (request):
+    response = send_request("/test/", "GET")
+    return HttpResponse(str(response), content_type="text/plain")
