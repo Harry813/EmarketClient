2b3f5d5f6ebc66c1948353baa35286238fd6e03a
HarryW <wangaoharry@hotmail.com>
2022-12-14 01:27:18 -0800

    Update:   - Update settings.py

--- EmarketClient/settings.py
+++ EmarketClient/settings.py
@@ -9,12 +9,14 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(verbose=True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -25,8 +27,8 @@ SECRET_KEY = 'django-insecure-k*c-&g8*3^_w)62am$ac&a=45m2$ii4di_8&89dc7j8k^%%_mc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['customer.hanxiaofei.me']
+CSRF_TRUSTED_ORIGINS = [f'https://customer.hanxiaofei.me/']
 
 # Application definition
 
@@ -70,17 +72,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'EmarketClient.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'EmarketClient',
+        'USER': 'EmarketClient',
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -100,7 +104,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -111,7 +114,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
