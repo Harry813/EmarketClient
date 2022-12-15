50ac74292f1bfabe88fea8978daf42e4f11cb6da
HarryW <wangaoharry@hotmail.com>
2022-12-15 14:41:08 -0800

    Update:   - Add API_ROOT to settings

--- EmarketClient/settings.py
+++ EmarketClient/settings.py
@@ -138,3 +138,6 @@ MEDIA_URL = 'media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Other settings
+API_ROOT = 'https://emarket.hanxiaofei.me/api/v1'
