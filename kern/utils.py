347cfbf5f2dfe50d48caac5e1cfdede9e2579d9b
HarryW <wangaoharry@hotmail.com>
2022-12-15 23:46:45 -0800

    Update:   - Add page title to get_client_params

--- kern/utils.py
+++ kern/utils.py
@@ -92,9 +92,10 @@ def v_record (request):
     )
 
 
-def get_client_params ():
+def get_client_params (page_title=None):
     return {
         "brand": os.getenv("BRAND"),
+        "title": f"{os.getenv('BRAND')}-{page_title}" if page_title else os.getenv("BRAND"),
     }
 
 
