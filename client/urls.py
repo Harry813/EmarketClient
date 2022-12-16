057f897761bca4dfaec564c57245e89efaf6e321
HarryW <wangaoharry@hotmail.com>
2022-12-15 23:42:17 -0800

    Update:   - Add urls to login, logout and register

--- client/urls.py
+++ client/urls.py
@@ -5,5 +5,10 @@ from . import views
 app_name = 'client'
 
 urlpatterns = [
-    path('', views.test, name='test'),
+    path('', views.index, name='index'),
+    path('test/', views.test, name='test'),
+
+    re_path(r'login/$', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),
 ]
