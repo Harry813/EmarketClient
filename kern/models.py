1cc546530d40cbcdf83abf92c55800ff2056ef75
HarryW <wangaoharry@hotmail.com>
2022-12-15 23:38:15 -0800

    Update:   - Change UserVisitRecord Model   - Rename User global_id to uuid   - Add User is_active   - Change restrictions of User Email field

--- kern/models.py
+++ kern/models.py
@@ -1,17 +1,18 @@
+import uuid
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
-    global_id = models.UUIDField()
-    email = models.EmailField(verbose_name='邮箱', max_length=60)
+    uuid = models.UUIDField(blank=True, null=True)
+    email = models.EmailField(verbose_name='邮箱', max_length=100, unique=True)
     username = models.CharField(verbose_name='用户名', max_length=30)
     first_name = models.CharField(verbose_name='名', max_length=30)
     last_name = models.CharField(verbose_name='姓', max_length=30)
     date_joined = models.DateTimeField(verbose_name='创建日期', auto_now_add=True)
     last_login = models.DateTimeField(verbose_name='上次登录', auto_now=True)
 
-    is_active = None
     is_superuser = None
     groups = None
     user_permissions = None
@@ -29,19 +30,26 @@ class User(AbstractUser):
 
 
 class UserVisitRecord(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='用户', blank=True, null=True)
-    ip = models.GenericIPAddressField(verbose_name='IP地址')
-    visit_time = models.DateTimeField(verbose_name='访问时间', auto_now_add=True)
-    uri = models.URLField(verbose_name='访问地址', max_length=200)
-    raw = models.JSONField(verbose_name='原始数据', blank=True, null=True)
+    user = models.ForeignKey(verbose_name="客户", on_delete=models.CASCADE, to="User", blank=True, null=True)
+    user_agent = models.CharField(verbose_name="用户代理", max_length=255, blank=True, null=True)
+    visited_at = models.DateTimeField(verbose_name="访问时间", auto_now_add=True)
+    ip = models.GenericIPAddressField(verbose_name="IP地址", blank=True, null=True)
+    referer = models.CharField(verbose_name="来源", max_length=255, blank=True, null=True)
+    path = models.CharField(verbose_name="路径", max_length=255, blank=True, null=True)
+    query_string = models.CharField(verbose_name="查询字符串", max_length=255, blank=True, null=True)
+    method = models.CharField(verbose_name="方法", max_length=255, blank=True, null=True)
+    is_synced = models.BooleanField(default=False)
 
     class Meta:
-        verbose_name = '用户访问记录'
-        verbose_name_plural = verbose_name
-        ordering = ['-visit_time']
+        verbose_name = "客户访问记录"
+        verbose_name_plural = "客户访问记录"
 
-    def __str__(self):
-        return f'{self.user.username if self.user else "Unknown"} - {self.ip}'
+    @property
+    def cust (self):
+        return self.user.full_name if self.user else "旅客"
+
+    def __str__ (self):
+        return f"{self.cust}[{self.ip}] @ {self.visited_at}"
 
 
 class Address(models.Model):
