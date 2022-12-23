1a647b2d43107330ab65b7912164516e14934ef2
HarryW <wangaoharry@hotmail.com>
2022-12-23 15:26:49 -0800

    Update:   - Add product detail url

--- client/urls.py
+++ client/urls.py
@@ -7,6 +7,7 @@ app_name = 'client'
 urlpatterns = [
     path('', views.index, name='index'),
     path('test/', views.test, name='test'),
+    path('product/<str:product_id>/', views.product_detail_view, name='product'),
 
     re_path(r'login/$', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
