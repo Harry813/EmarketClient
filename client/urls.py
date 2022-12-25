ad0477ac24c116da11502f8829b12da5cf06d9be
HarryW <wangaoharry@hotmail.com>
2022-12-25 15:15:22 -0800

    Update:   - Add products url   - Add wishlist url

--- client/urls.py
+++ client/urls.py
@@ -7,7 +7,11 @@ app_name = 'client'
 urlpatterns = [
     path('', views.index, name='index'),
     path('test/', views.test, name='test'),
+
+    path('product/', views.products_view, name='shop'),
     path('product/<str:product_id>/', views.product_detail_view, name='product'),
+
+    path('wishlist/', views.wishlist_view, name='wishlist'),
 
     re_path(r'login/$', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
