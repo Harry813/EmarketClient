dd5cce2e7635956858e1e7194f05b95ad787c41d
HarryW <wangaoharry@hotmail.com>
2022-12-27 13:07:46 -0800

    Update:   - Add cart related api   - Change the auto-refresh to 1.5s

--- kern/views.py
+++ kern/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.shortcuts import render
 from django.utils.translation import gettext as _
 from django.views.decorators.csrf import csrf_exempt
@@ -26,4 +28,61 @@ def wishlist_api (request, format=None):
         if variant_id is None:
             return Response({"ERR": "No variant id provided"}, status=HTTP_400_BAD_REQUEST)
         WishlistItem.objects.filter(user=user, variant_id=variant_id).delete()
-        return Response({"msg": _('删除成功，2秒后刷新页面')}, status=HTTP_200_OK)
+        return Response({"msg": _('删除成功，1.5秒后刷新页面')}, status=HTTP_200_OK)
+
+
+@csrf_exempt
+@api_view(['GET', 'POST', 'PUSH', 'DELETE'])
+def cart_api (request):
+    if request.method == "GET":
+        items = CartItem.objects.filter(user=request.user)
+        total_price = sum([item.variant.price * item.quantity for item in items])
+        return Response({"items": items, "total_price": total_price}, status=HTTP_200_OK)
+    elif request.method == "POST":
+        # 至多一件商品
+        user = User.objects.get(pk=request.user.pk)
+        variant_id = request.data.get("id", None)
+        quantity = request.data.get("quantity")
+        obj, status = CartItem.objects.get_or_create(user=user, variant_id=variant_id)
+        if not status:
+            obj.quantity += quantity
+            obj.save()
+            return Response({"msg": _("商品添加成功，购物车中有 %(count)s 件") % {'count': obj.quantity}},
+                            status=HTTP_200_OK)
+        return Response({"msg": _("添加成功")}, status=HTTP_201_CREATED)
+    elif request.method == "PUSH":
+        user = User.objects.get(pk=request.user.pk)
+        mode = request.data.get("mode", None)
+        if mode == "full_update":
+            items = request.data.get("items", None)
+            if items is None:
+                return Response({"msg": _("无效的请求")}, status=HTTP_400_BAD_REQUEST)
+            items = json.loads(items)
+            for item in items:
+                variant_id = item.get("id", None)
+                quantity = item.get("quantity", None)
+                if variant_id is None or quantity is None:
+                    return Response({"msg": _("无效的请求")}, status=HTTP_400_BAD_REQUEST)
+
+                obj, status = CartItem.objects.get_or_create(user=user, variant_id=variant_id)
+                if not status:
+                    obj.quantity = quantity
+                    obj.save()
+            return Response({"msg": _("更新成功, 1.5秒后将刷新页面")}, status=HTTP_200_OK)
+        else:
+            variant_id = request.data.get("variants")
+            quantity = request.data.get("quantity")
+            obj, status = CartItem.objects.get_or_create(user=user, variant_id=variant_id)
+            if not status:
+                obj.quantity = quantity
+                obj.save()
+                return Response({"msg": _("商品修改成功, 1.5秒后将刷新页面") % {'count': obj.quantity}},
+                                status=HTTP_200_OK)
+        return Response({"ERR": "Unknown err"}, status=HTTP_400_BAD_REQUEST)
+    elif request.method == "DELETE":
+        user = User.objects.get(pk=request.user.pk)
+        variant_id = request.query_params.get("id")
+        if variant_id is None:
+            return Response({"ERR": _("无效的请求")}, status=HTTP_400_BAD_REQUEST)
+        CartItem.objects.filter(user=user, variant_id=variant_id).delete()
+        return Response({"msg": _('删除成功，1.5秒后刷新页面')}, status=HTTP_200_OK)
