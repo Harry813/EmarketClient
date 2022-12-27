8844f7087240c56056188e23f6cf8b1260b48c3b
HarryW <wangaoharry@hotmail.com>
2022-12-26 16:48:04 -0800

    Update:   - change the default info provide to each view

--- kern/utils/utils.py
+++ kern/utils/utils.py
@@ -1,8 +1,20 @@
 import os
 
+from kern.models import CartItem
 
-def get_client_params (page_title=None):
-    return {
+
+def get_client_params (request, page_title=None):
+    data = {
         "brand": os.getenv("BRAND"),
         "title": f"{os.getenv('BRAND')}-{page_title}" if page_title else os.getenv("BRAND"),
     }
+    if request.user.is_authenticated:
+        cart_items = CartItem.objects.filter(user_id=request.user.id)
+        total = sum([item.total for item in cart_items])
+        quantity = sum([item.quantity for item in cart_items])
+        data.update({
+            "cart": cart_items,
+            "cart_subtotal": total,
+            "cart_count": quantity,
+        })
+    return data
