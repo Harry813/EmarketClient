0ba83170b05869b38038206eff973fbb0c860228
HarryW <wangaoharry@hotmail.com>
2023-01-01 14:36:44 -0800

    Update:   - Add checkout, pay and pay finish url

--- client/urls.py
+++ client/urls.py
@@ -13,6 +13,9 @@ urlpatterns = [
 
     path('wishlist/', views.wishlist_view, name='wishlist'),
     path('cart/', views.cart_view, name='cart'),
+    path('checkout/', views.checkout_view, name='checkout'),
+    path('pay/<str:order_id>/', views.pay_view, name='pay'),
+    path('pay/finish/<str:order_id>/', views.pay_finish, name='pay_finish'),
 
     re_path(r'login/$', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
