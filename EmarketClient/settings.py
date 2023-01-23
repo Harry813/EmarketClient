a651e0de7cd8e345edfc0af4bd471ffd68e68e5e
HarryW <wangaoharry@hotmail.com>
2023-01-22 20:21:51 -0800

    Update:   - settings.py will now take database User and Port from .env file

--- EmarketClient/settings.py
+++ EmarketClient/settings.py
@@ -93,10 +93,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'EmarketClient',
-        'USER': 'EmarketClient',
+        'USER': os.getenv('DB_USER', 'EmarketClient'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv("DB_HOST", '127.0.0.1'),
-        'PORT': '3306',
+        'PORT': os.getenv("DB_PORT", '3306'),
     }
 }
 
