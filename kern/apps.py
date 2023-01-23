f757161cc4ce72cae63b2cf242b80ae8e242aec7
HarryW <wangaoharry@hotmail.com>
2023-01-23 14:19:38 -0800

    Bug Fix：   - Remove unnecessary codes

--- kern/apps.py
+++ kern/apps.py
@@ -5,6 +5,3 @@ from django.utils.module_loading import autodiscover_modules
 class KernConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'kern'
-
-    def ready (self):
-        autodiscover_modules('tasks')
