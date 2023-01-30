cc9568366f32e9f09b64e813e5ca9a15c417b1c1
HarryW <wangaoharry@hotmail.com>
2023-01-29 16:07:13 -0800

    :sparkles: Update apps.py   - Add initiation code when first time run

--- kern/apps.py
+++ kern/apps.py
@@ -1,7 +1,20 @@
 from django.apps import AppConfig
-from django.utils.module_loading import autodiscover_modules
 
 
 class KernConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'kern'
+
+    def ready (self):
+        from rest_framework_api_key.models import APIKey
+        from kern.utils.core import send_request
+
+        if not APIKey.objects.filter(name='ADM_CONTROL').exists():
+            response = send_request('/init/api-key/', 'GET', payload={"name": "ADM_CONTROL"})
+            if response.status_code == 200:
+                APIKey.objects.create(**response.json())
+
+        if not APIKey.objects.filter(name='RMTCTL').exists():
+            response = send_request('/init/api-key/', 'GET', payload={"name": "RMTCTL"})
+            if response.status_code == 200:
+                APIKey.objects.create(**response.json())
