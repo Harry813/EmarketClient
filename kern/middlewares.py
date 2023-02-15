73fb210537c6c4d131bc72a1a2bd1f29515caac5
HarryW <wangaoharry@hotmail.com>
2023-02-15 11:47:13 -0800

    :hammer: Optimize import

--- kern/middlewares.py
+++ kern/middlewares.py
@@ -1,10 +1,6 @@
-import os
-
-from django.core.exceptions import PermissionDenied
 from django.utils.deprecation import MiddlewareMixin
 
 from kern.utils.auth import v_record
-from kern.utils.core import send_request
 
 
 class ClientMiddleware(MiddlewareMixin):
