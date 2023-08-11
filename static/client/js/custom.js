0438d6d1d59eeafc8e8cf98b94d2fc4bc55f355c
HarryW <wangaoharry@hotmail.com>
2023-08-11 14:31:24 +0800

    :bug: Fix:   - Add missing functions

--- static/client/js/custom.js
+++ static/client/js/custom.js
@@ -49,3 +49,146 @@ const notyf = new Notyf({
         }
     ]
 });
+
+$(document).on('click', 'a.add_wishlist, button.add_wishlist', function (e) {
+    e.preventDefault();
+    let $this = $(this);
+    let variant_id = $this.data('variant');
+
+    $.ajax({
+        url: kern_wishlist,
+        type: 'POST',
+        data: {
+            id: variant_id,
+        },
+        headers: {
+            'X-CSRFToken': csrfToken
+        },
+        success: function (data) {
+            notyf.success(data["msg"]);
+        },
+        error: function (data) {
+            if (data.responseJSON["detail"] === "Authentication credentials were not provided.") {
+                notyf.error('{% trans "用户尚未登录，无法使用该功能" %}');
+            } else {
+                notyf.error(data["msg"]);
+            }
+        }
+    });
+});
+
+$(document).on('click', 'a.remove_wishlist, button.remove_wishlist', function (e) {
+    e.preventDefault();
+    let $this = $(this);
+    let variant_id = $this.data('variant');
+
+    $.ajax({
+        url: kern_wishlist + "?id=" + variant_id,
+        type: 'DELETE',
+        data: {
+            id: variant_id,
+        },
+        headers: {
+            'X-CSRFToken': csrfToken
+        },
+        success: function (data) {
+            console.log(data);
+            notyf.success(data["msg"]);
+            setTimeout(function () {
+                window.location.reload();
+            }, 1500);
+        },
+        error: function (data) {
+            if (data.responseJSON["detail"] === "Authentication credentials were not provided.") {
+                notyf.error('{% trans "用户尚未登录，无法使用该功能" %}');
+            } else {
+                notyf.error(data["msg"]);
+            }
+        }
+    });
+});
+
+$(document).on('click', 'a.add_cart, button.add_cart', function (e) {
+    e.preventDefault();
+    let $this = $(this);
+    let variant_id = $this.data('variant');
+
+    $.ajax({
+        url: kern_cart,
+        type: 'POST',
+        data: {
+            id: variant_id,
+        },
+        headers: {
+            'X-CSRFToken': csrfToken
+        },
+        success: function (data) {
+            notyf.success(data["msg"]);
+        },
+        error: function (data) {
+            console.log(data)
+            if (data.responseJSON["detail"] === "Authentication credentials were not provided.") {
+                notyf.error('{% trans "用户尚未登录，无法使用该功能" %}');
+            } else {
+                notyf.error(data["msg"]);
+            }
+        }
+    });
+});
+
+$(document).on('click', 'a.remove_cart, button.remove_cart', function (e) {
+    e.preventDefault();
+    let $this = $(this);
+    let variant_id = $this.data('variant');
+
+    $.ajax({
+        url: kern_cart + "?id=" + variant_id,
+        type: 'DELETE',
+        headers: {
+            'X-CSRFToken': csrfToken
+        },
+        success: function (data) {
+            console.log(data);
+            notyf.success(data["msg"]);
+            setTimeout(function () {
+                window.location.reload();
+            }, 1500);
+        },
+        error: function (data) {
+            console.log(data)
+            if (data.responseJSON["detail"] === "Authentication credentials were not provided.") {
+                notyf.error('{% trans "用户尚未登录，无法使用该功能" %}');
+            } else {
+                notyf.error(data["msg"]);
+            }
+        }
+    });
+});
+
+$(document).on('submit', 'form.add_cart_form', function (e) {
+    e.preventDefault();
+    let $this = $(this);
+    let $quantity = $this.find('input[name="quantity"]');
+    let variant_id = $quantity.data('variant');
+    $.ajax({
+        url: kern_cart,
+        type: 'POST',
+        data: {
+            id: variant_id,
+            quantity: $quantity,
+        },
+        headers: {
+            'X-CSRFToken': csrfToken
+        },
+        success: function (data) {
+            notyf.success(data["msg"]);
+        },
+        error: function (data) {
+            if (data.responseJSON["detail"] === "Authentication credentials were not provided.") {
+                notyf.error('{% trans "用户尚未登录，无法使用该功能" %}');
+            } else {
+                notyf.error(data["msg"]);
+            }
+        }
+    });
+});
