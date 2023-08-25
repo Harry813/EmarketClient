ac901a7fbb18c32ba2e335d14f752611bb881394
HarryW <wangaoharry@hotmail.com>
2023-08-25 11:13:17 +0800

    :art:

--- kern/models.py
+++ kern/models.py
@@ -20,18 +20,18 @@ class RemoteModel(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     @property
-    def is_expired (self):
+    def is_expired(self):
         diff = datetime.datetime.now(datetime.timezone.utc) - self.updated_at
         return diff > datetime.timedelta(hours=1)
 
     def update(self):
-        response = send_request(f"/{self.URL}/", "GET", {"mode": "retrieve", "id": str(self.id)})
+        response = send_request(f"/{self.URL}/", "GET", {"mode": "retrieve", "id": str(self.pk)})
         try:
             response.raise_for_status()
         except HTTPError:
             # todo: 添加日志
             # todo: 添加开发人员警报
-            logging.warning(f"Failed to update {self.__class__.__name__} {self.id}")
+            logging.warning(f"Failed to update {self.__class__.__name__} {self.pk}")
             logging.info(response.text)
             return None
 
@@ -43,12 +43,12 @@ class RemoteModel(models.Model):
         else:
             return None
 
-    def save (self, *args, **kwargs):
+    def save(self, *args, **kwargs):
         self.updated_at = datetime.datetime.now()
         super().save(*args, **kwargs)
 
     @property
-    def data (self):
+    def data(self):
         if self.is_expired or self.raw_data == {}:
             self.update()
         return self.raw_data
@@ -89,15 +89,15 @@ class User(AbstractUser):
     REQUIRED_FIELDS = ['username']
 
     @property
-    def full_name (self):
+    def full_name(self):
         return self.first_name + self.last_name
 
     @property
-    def wishlist (self):
+    def wishlist(self):
         return WishlistItem.objects.filter(user=self)
 
     @property
-    def short_id (self):
+    def short_id(self):
         return str(self.id)[:6]
 
     class Meta:
@@ -109,43 +109,43 @@ class Address(RemoteModel):
     URL = "address"
 
     @property
-    def first_name (self):
+    def first_name(self):
         return self.data.get("first_name", "")
 
     @property
-    def last_name (self):
+    def last_name(self):
         return self.data.get("last_name", "")
 
     @property
-    def full_name (self):
+    def full_name(self):
         return f"{self.first_name} {self.last_name}"
 
     @property
-    def address_line1 (self):
+    def address_line1(self):
         return self.data.get("address_line1", "")
 
     @property
-    def address_line2 (self):
+    def address_line2(self):
         return self.data.get("address_line2", "")
 
     @property
-    def city (self):
+    def city(self):
         return self.data.get("city", "")
 
     @property
-    def state (self):
+    def state(self):
         return self.data.get("state", "")
 
     @property
-    def country (self):
+    def country(self):
         return self.data.get("country", "")
 
     @property
-    def zip_code (self):
+    def zip_code(self):
         return self.data.get("zip_code", "")
 
     @property
-    def phone (self):
+    def phone(self):
         return self.data.get("phone", "")
 
 
@@ -153,56 +153,56 @@ class Product(RemoteModel):
     URL = "product"
 
     @property
-    def name (self):
+    def name(self):
         return self.data.get("name", "")
 
     @property
-    def short_description (self):
+    def short_description(self):
         return self.data.get("short_description", "")
 
     @property
-    def description (self):
+    def description(self):
         return self.data.get("description", "")
 
     @property
-    def meta_keywords (self):
+    def meta_keywords(self):
         return self.data.get("meta_keywords", "")
 
     @property
-    def meta_description (self):
+    def meta_description(self):
         return self.data.get("meta_description", "")
 
     @property
-    def images (self):
+    def images(self):
         imgs = self.data.get("images", [])
         return [Image.objects.get(id=img) for img in imgs]
 
     @property
-    def thumbnails (self):
+    def thumbnails(self):
         imgs = self.data.get("thumbnails", [])
         return [Image.objects.get(id=img) for img in imgs]
 
     @property
-    def both_images (self):
+    def both_images(self):
         data = []
         for img, thumbnail in zip(self.images, self.thumbnails):
             data.append({"image": img, "thumbnail": thumbnail})
         return data
 
     @property
-    def cover (self):
+    def cover(self):
         return self.images[0]
 
     @property
-    def cover_thumbnail (self):
+    def cover_thumbnail(self):
         return self.thumbnails[0]
 
     @property
-    def variants (self):
+    def variants(self):
         return [ProductVariant.objects.get(id=variant) for variant in self.data.get("variants", [])]
 
     @property
-    def variant (self):
+    def variant(self):
         if self.variants:
             return self.variants[0]
         return None
@@ -212,16 +212,16 @@ class ProductVariant(RemoteModel):
     URL = 'variant'
 
     @property
-    def price (self):
+    def price(self):
         # float to decimal, 2 decimal places
         return Decimal(self.data.get("price", 0)).quantize(Decimal("0.00"))
 
     @property
-    def original_price (self):
+    def original_price(self):
         return Decimal(self.data.get("original_price", 0)).quantize(Decimal("0.00"))
 
     @property
-    def product (self):
+    def product(self):
         return Product.objects.get(id=self.data.get("product"))
 
 
@@ -237,27 +237,27 @@ class CartItem(models.Model):
         verbose_name_plural = "购物车商品"
 
     @property
-    def name (self):
+    def name(self):
         return self.variant.product.name
 
     @property
-    def price (self):
+    def price(self):
         # float to decimal, 2 decimal places
         return Decimal(self.variant.price.quantize(Decimal("0.00")))
 
     @property
-    def total (self):
+    def total(self):
         return self.price * self.quantity
 
     @property
-    def image (self):
+    def image(self):
         return self.variant.product.cover_thumbnail
 
     @property
-    def product (self):
+    def product(self):
         return self.variant.product
 
-    def save (self, *args, **kwargs):
+    def save(self, *args, **kwargs):
         if self.quantity == 0:
             self.delete()
         else:
@@ -265,7 +265,7 @@ class CartItem(models.Model):
 
 
 class ImageManager(models.Manager):
-    def __delete__ (self, instance):
+    def __delete__(self, instance):
         instance.img.delete()
         super().__delete__(instance)
 
@@ -277,11 +277,11 @@ class Image(models.Model):
     alt = models.CharField(verbose_name="图片描述", max_length=255, blank=True, null=True)
     updated_at = models.DateTimeField(verbose_name="最后更新时间", blank=True, null=True)
 
-    def __str__ (self):
+    def __str__(self):
         return f"{self.id}"
 
     @property
-    def url (self):
+    def url(self):
         return self.img.url
 
 
@@ -291,7 +291,7 @@ class WishlistItem(models.Model):
     created_at = models.DateTimeField(verbose_name="创建时间", auto_now_add=True)
 
     @property
-    def product (self):
+    def product(self):
         return self.variant.product
 
     class Meta:
@@ -327,74 +327,74 @@ class Order(RemoteModel):
     user = models.ForeignKey(verbose_name="客户", on_delete=models.CASCADE, to="User")
     is_active = models.BooleanField(verbose_name="是否激活", default=True)
 
-    def __str__ (self):
+    def __str__(self):
         return f"ORD #{str(self.id)[-6:]}"
 
     @property
-    def items (self):
+    def items(self):
         return self.data.get("items", [])
 
     @property
-    def quantity (self):
+    def quantity(self):
         return sum([item["quantity"] for item in self.items])
 
     @property
-    def subtotal (self):
+    def subtotal(self):
         return Decimal(self.data.get("subtotal", 0)).quantize(Decimal("0.00"))
 
     @property
-    def shipping (self):
+    def shipping(self):
         return Decimal(self.data.get("shipping", 0)).quantize(Decimal("0.00"))
 
     @property
-    def shipment (self):
+    def shipment(self):
         return self.data.get("shipment", {})
 
     @property
-    def carrier (self):
+    def carrier(self):
         if self.shipment:
             return self.shipment.get("carrier")
         return
 
     @property
-    def payment (self):
+    def payment(self):
         if self.status in ["PAID", "PROCESSING", "CONFIRMED", "SHIPPED", "COMPLETED"]:
             return self.data.get("payment", {})
         return None
 
     @property
-    def discount (self):
+    def discount(self):
         return Decimal(self.data.get("discount", 0)).quantize(Decimal("0.00"))
 
     @property
-    def total (self):
+    def total(self):
         return Decimal(self.data.get("total", 0)).quantize(Decimal("0.00"))
 
     @property
-    def date (self):
+    def date(self):
         created_at = self.data.get("created_at")
         return created_at.split("T")[0]
 
     @property
-    def status (self):
+    def status(self):
         return self.data.get("status")
 
-    def get_status_display (self):
+    def get_status_display(self):
         return self.STATUS.get(self.status, self.status)
 
     @property
-    def short_id (self):
+    def short_id(self):
         return str(self.id)[-6:]
 
     @property
-    def billing_address (self):
+    def billing_address(self):
         return Address.objects.filter(id=self.data.get("billing_address")).first()
 
     @property
-    def shipping_address (self):
+    def shipping_address(self):
         return Address.objects.filter(id=self.data.get("shipping_address")).first()
 
-    def save (self, *args, **kwargs):
+    def save(self, *args, **kwargs):
         if self.is_active:
             Order.objects.filter(user=self.user, is_active=True).update(is_active=False)
         super().save(*args, **kwargs)
@@ -428,3 +428,33 @@ class ErrorLog(models.Model):
             return True
         else:
             return False
+
+
+class Wallet(RemoteModel):
+    URL = "wallet"
+
+    id = None
+    customer = models.ForeignKey(verbose_name="客户", on_delete=models.CASCADE, to="User", primary_key=True)
+
+    @property
+    def available_balance(self):
+        return Decimal(self.data.get("available_balance", 0)).quantize(Decimal("0.00"))
+
+    @property
+    def frozen_balance(self):
+        return Decimal(self.data.get("frozen_balance", 0)).quantize(Decimal("0.00"))
+
+    @property
+    def bind_balance(self):
+        return Decimal(self.data.get("bind_balance", 0)).quantize(Decimal("0.00"))
+
+
+class Point(RemoteModel):
+    URL = "point"
+
+    id = None
+    customer = models.ForeignKey(verbose_name="客户", on_delete=models.CASCADE, to="User", primary_key=True)
+
+    @property
+    def point(self):
+        return self.data.get("point", 0)
