e82bbd0226093d954920c93542414930213cc1d0
HarryW <wangaoharry@hotmail.com>
2023-09-03 17:02:07 +0800

    :zap: Update:   - Add password reset and point exchange URLs

--- kern/urls.py
+++ kern/urls.py
@@ -8,7 +8,8 @@ in_site_api = [
     path('wishlist/', views.wishlist_api, name='wishlist'),
     path('cart/', views.cart_api, name='cart'),
     path('cart/checkout/', views.checkout_api, name='checkout'),
-    path("user/resetpaswd/", views.reset_password, name="reset_password")
+    path("user/resetpaswd/", views.reset_password, name="reset_password"),
+    path("point/exchange/", views.point_exchange, name="point_exchange"),
 ]
 
 out_site_api = [
